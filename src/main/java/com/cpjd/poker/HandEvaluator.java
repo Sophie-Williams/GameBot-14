5e7859046d4b0726ad5ab468ddb6c5bd914aff3f
WillDavies <wdavies973@gmail.com>
2018-06-17 18:11:28 -0500

    Added more code

--- src/main/java/com/cpjd/poker/HandEvaluator.java
+++ src/main/java/com/cpjd/poker/HandEvaluator.java
@@ -1,8 +1,11 @@
 package com.cpjd.poker;
 
 import com.cpjd.models.Card;
+import com.cpjd.models.NUMBER;
 import com.cpjd.models.Player;
+import com.cpjd.models.SUIT;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class HandEvaluator {
@@ -21,10 +24,91 @@ public class HandEvaluator {
      */
     public HandValue computePlayerValue(Card card1, Card card2) {
         ArrayList<HandValue> potentialValues = new ArrayList<>();
+
+        ArrayList<ArrayList<Card>> combos = getCombinations(card1, card2);
     }
 
-    private HandValue computeHandValue(Card card1, Card card2) {
+    private HandValue computeHandValue(ArrayList<Card> cards) {
 
+    }
+
+    /*
+     * Quality - these assume types
+     */
+    public int getStraightFlushQuality(ArrayList<Card> cards) {
+        return getHighest(cards);
+    }
+
+    public int getFourOfAKindQuality(ArrayList<Card> cards) {
+        return getHighest(cards);
+    }
+
+    /*
+     * Types
+     */
+    private boolean isStraightFlush(ArrayList<Card> cards) {
+        return sameSuit(cards) && ascending(cards);
+    }
+
+    private boolean isFourOfAKind(ArrayList<Card> cards) {
+        Card c = cards.get(0);
+        int number = 0;
+        for(Card s : cards) {
+            if(c.getSuit().equals(s.getSuit())) number++;
+        }
+        if(number == 4) return true;
+        number = 0;
+        c = cards.get(1);
+        for(Card s : cards) {
+            if(c.getSuit().equals(s.getSuit())) number++;
+        }
+        if(number == 4) return true;
+        return false;
+    }
+
+    private boolean sameSuit(ArrayList<Card> cards) {
+        SUIT suit = cards.get(0).getSuit();
+        for(Card c : cards) if(c.getSuit() != suit) return false;
+        return true;
+    }
+
+    private static int getHighest(ArrayList<Card> cards) {
+        int highest = cards.get(0).getNumber().getNumerical(true);
+
+        for(Card c : cards) {
+            if(c.getNumber().getNumerical(true) > highest) highest = c.getNumber().getNumerical(true);
+        }
+
+        return highest;
+    }
+
+    private static boolean ascending(ArrayList<Card> cards) {
+        return isAscendingAceHigh(cards, true);
+    }
+
+    private static boolean isAscendingAceHigh(ArrayList<Card> cards, boolean aceHigh) {
+        // Find lowest number (assume aces are 1)
+        int lowest = cards.get(0).getNumber().getNumerical(aceHigh);
+        for (Card c : cards) {
+            if (c.getNumber().getNumerical(aceHigh) < lowest) lowest = c.getNumber().getNumerical(aceHigh);
+        }
+
+        // Now, cards must contain increasing levels
+        boolean ascending = true;
+        for (int i = 0, search = lowest; i < cards.size(); i++) {
+            if (cards.get(i).getNumber().getNumerical(aceHigh) == search) {
+                // Found a card, increment
+                search++;
+            }
+
+            if (i == cards.size() && search != lowest + 4) {
+                ascending = false;
+            }
+        }
+
+        if(!aceHigh) return ascending; // prevent stack overflow
+
+        return ascending || isAscendingAceHigh(cards, false);
     }
 
     private ArrayList<ArrayList<Card>> getCombinations(Card card1, Card card2) {
---
+++ src/test/java/com/cpjd/poker/HandEvaluatorTest.java
@@ -0,0 +1,12 @@
+package com.cpjd.poker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HandEvaluatorTest {
+    @Test
+    public void isAscending() {
+
+    }
+}
